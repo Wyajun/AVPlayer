26c577f59fdee588988a1095f5b55268d4b07dce
wangyajun <yajunst@163.com>
2016-11-11 11:50:53 +0800

    Initial Commit

---
+++ 视频播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  视频播放器
+//
+//  Created by 王亚军 on 16/11/11.
+//  Copyright © 2016年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 视频播放器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  视频播放器
+//
+//  Created by 王亚军 on 16/11/11.
+//  Copyright © 2016年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
